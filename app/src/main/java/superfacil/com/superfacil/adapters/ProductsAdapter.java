52f34e0d5e4fc0852889699e6c080b0c4051c718
Edgar Pérez <edgar@mail.com>
2016-04-23 21:25:41 -0500

    Cambios

--- app/src/main/java/superfacil/com/superfacil/adapters/ProductsAdapter.java
+++ app/src/main/java/superfacil/com/superfacil/adapters/ProductsAdapter.java
@@ -1,10 +1,14 @@
 package superfacil.com.superfacil.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -16,26 +20,42 @@ import superfacil.com.superfacil.model.Product;
  */
 public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHolder> {
 
-    public List<Product> mProducts;
+    private List<Product> mProducts;
+    private Context mContext;
 
-    public Pro
+    public ProductsAdapter(List<Product> products, Context context){
+        this.mProducts = products;
+        this.mContext = context;
+    }
 
+    public void setProducts(List<Product> products){
+        mProducts = products;
+        notifyDataSetChanged();
+    }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
+        ViewHolder viewHolder = new ViewHolder(itemView);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        Product product = mProducts.get(position);
 
+        Glide.with(mContext).load(product.getImage()).centerCrop().crossFade().into(holder.imageProduct);
+        holder.productTitle.setText(product.getNombre());
+        holder.productPrice.setText(String.valueOf(product.getPrecio()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mProducts.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public ImageView imageProduct;
         public TextView productTitle;
@@ -48,6 +68,13 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ViewHo
             imageProduct = (ImageView) itemView.findViewById(R.id.image_product);
             productTitle = (TextView) itemView.findViewById(R.id.name_product);
             productPrice = (TextView) itemView.findViewById(R.id.price_product);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+
         }
     }
 }
--- app/src/main/java/superfacil/com/superfacil/fragments/SearchFragment.java
+++ app/src/main/java/superfacil/com/superfacil/fragments/SearchFragment.java
@@ -3,12 +3,27 @@ package superfacil.com.superfacil.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.internal.util.Predicate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import superfacil.com.superfacil.R;
+import superfacil.com.superfacil.adapters.ProductsAdapter;
+import superfacil.com.superfacil.model.Product;
+import superfacil.com.superfacil.utilities.DataHelper;
 
 /**
  * Created by edgarperez on 4/23/16.
@@ -16,6 +31,8 @@ import superfacil.com.superfacil.R;
 public class SearchFragment extends Fragment {
 
     private RecyclerView mProducts;
+    private ProductsAdapter mProductsAdapter;
+    private List<Product> mListProducts;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,5 +53,56 @@ public class SearchFragment extends Fragment {
 
     private void InitViews(View rootView){
         mProducts = (RecyclerView) rootView.findViewById(R.id.products_recycler_view);
+
+        mListProducts = DataHelper.getFakeData(getActivity());
+
+        mProductsAdapter = new ProductsAdapter(mListProducts, getActivity());
+
+        mProducts.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mProducts.setAdapter(mProductsAdapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        MenuItem item = menu.add("Search");
+        item.setIcon(R.drawable.ic_search);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        SearchView sv = new SearchView(getActivity());
+        sv.setIconified(false);
+
+        int id = sv.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        TextView textView = (TextView) sv.findViewById(id);
+        textView.setHint("Search product...");
+
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(final String query) {
+                final List<Product> filteredModelList = filter(mListProducts, query);
+                if (mProductsAdapter != null){
+                    mProductsAdapter.setProducts(filteredModelList);
+                }
+                return true;
+            }
+        });
+        item.setActionView(sv);
+    }
+
+    private List<Product> filter(List<Product> models, String query) {
+        query = query.toLowerCase();
+
+        final List<Product> filteredModelList = new ArrayList<>();
+        for (Product model : models) {
+            final String text = model.getNombre().toLowerCase();
+            if (text.contains(query)) {
+                filteredModelList.add(model);
+            }
+        }
+        return filteredModelList;
     }
 }
